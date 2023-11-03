5dbfa22c2fe37d096b4f3539bf08fe467bf6b9a9
Chad Harper <chadharper811@gmail.com>
2023-11-02 23:12:13 -0500

    added code for task 3 still not displaying any jobs in search, needs more work

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,16 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.awt.im.spi.InputMethod;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -15,13 +22,29 @@ import static org.launchcode.techjobsmvc.controllers.ListController.columnChoice
 @RequestMapping("search")
 public class SearchController {
 
+    private static List<String> jobs = new ArrayList<>();
+
     @GetMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping(value="")
+    public String displaySearchResults(@RequestParam String searchType, @RequestParam String searchTerm, Model model) {
+        String searchJobs;
+        List<String> jobs = new ArrayList<>();
+        if (searchTerm.equals("") || searchTerm.toLowerCase().equals("all")) {
+            searchJobs = String.valueOf(JobData.findAll());
+        } else {
+            searchJobs = String.valueOf(JobData.findByColumnAndValue(searchType, searchTerm));
+        }
+        jobs.add(searchJobs);
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("searchTerm", searchTerm);
+        model.addAttribute("jobs", jobs);
+        return "redirect:/search";
+    }
 
 }
 
